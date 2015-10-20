fe522bcaf1a522fe3a4657d7f2ae2006d3f99cd6
Ethan Riley <riley787cmplex@gmail.com>
2015-10-20 19:52:56 +0000

    got rid of :target using jquery instead

--- _scripts/js/responsive.js
+++ _scripts/js/responsive.js
@@ -1,8 +1,22 @@
-function focushaze(ElementFocused, Elementhaze){
-      
+function show(element){
+  $(element).css('display', 'block');
+}
+function hide(element){
+  $(element).css('display', 'none');
 }
 
 
+function navbaropen(){
+    show('.accordion-content');
+    $('.accordion-content').css('overflow', 'auto');
+    show('.overlay');
+}
+
+function navbarclose(){
+    hide('.accordion-content');
+    $('.accordion-content').css('overflow', 'hidden');
+    hide('.overlay');
+}
 
 $(document).ready(function() {
   var mainMarginTop = $('#title').height()*2;
