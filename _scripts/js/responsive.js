4708a8718319e53597be4223b7054962f735ca57
Ethan Riley <riley787cmplex@gmail.com>
2015-10-20 19:21:44 +0000

    fixed media queries, added haze to menu

--- _scripts/js/responsive.js
+++ _scripts/js/responsive.js
@@ -1,3 +1,9 @@
+function focushaze(ElementFocused, Elementhaze){
+      
+}
+
+
+
 $(document).ready(function() {
   var mainMarginTop = $('#title').height()*2;
   $('main').css('margin-top', mainMarginTop+"px");
