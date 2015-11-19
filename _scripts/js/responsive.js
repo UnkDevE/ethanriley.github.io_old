0612a134a2f64090fad8dafd4cecbd98a42754f0
Ethan Riley <mbs014769@meole.co.uk>
2015-11-19 16:10:25 +0000

    added gallery internals

--- _scripts/js/responsive.js
+++ _scripts/js/responsive.js
@@ -18,6 +18,23 @@ function extendElipsisCutoff(classname){
       }
     );
 }
+function mainmedia(type, srclink){
+  var id="#main_"+type;
+  if(id !== "#main_img"){
+    $("#main_img").css('display','none');
+  }
+  if(id !== "#main_video"){
+    $("#main_video").css('display','none');
+  }
+  else{
+    $("#main_iframe").css('display','none');
+  }
+  if(id === "#main_iframe"){
+    srclink = "http://"+srclink;
+  }
+  $(id).attr('src', srclink);
+  $(id).css('display', 'block');
+}
 
 function onlink(link){
   window.location.assign(link);
@@ -28,6 +45,10 @@ $(document).ready(function() {
   $('#main').css('margin-top', mainMarginTop+"px");
   extendElipsisCutoff(".card-elipsis");
 
-  $('.card-gallery-bar').css('width')
+  $('.card-gallery-bar').css('width');
+  $('button').click(function(){
+    $('.card-important').append("<p>thankyou for your custom we shall get back to you shortly</p>");
+    }
+  )
 }
 )
